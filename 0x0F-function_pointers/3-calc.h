65c60a07e3972573e66f7a52a4dc8362e3e5a99c
chikaazubu2 <chikagnwaigwe@gmail.com>
2023-01-19 04:47:46 -0800

    task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -6,7 +6,7 @@
 /**
  * struct op - Struct op
  *
- * op: The operator
+ * @op: The operator
  * @f: The function associated
  */
 typedef struct op
@@ -14,7 +14,6 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
